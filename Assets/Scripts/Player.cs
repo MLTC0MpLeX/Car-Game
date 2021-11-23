65ec4d7f309e2b83c1719a485cfea1f034ddf2b8
MLTC0MpLeX <michaeljscerri@gmail.com>
2021-11-23 13:17:54 +0100

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1;
+    float xMin, xMax, yMin, yMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    
+    private void SetUpMoveBoundaries()
+    {
+        //get the Main Camera from Unity
+        Camera gameCamera = Camera.main;
+
+        //set the boundaries on the x-axis
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding + 5;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding - 5;
+
+        //set the boundaries on the y-axis
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding - 7;
+    }
+
+    void Move()
+    {
+               
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        //newXPos   = current x-position   + difference in x
+        var newXPos = transform.position.x + deltaX;
+        //apply boundaries on x-axis
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        //the above in y axis:
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+        newYPos = Mathf.Clamp(newYPos, yMin, yMax);
+
+        //move the Player ship to the newXPos
+        this.transform.position = new Vector2(newXPos, newYPos);
+        
+                
+    }
+}
