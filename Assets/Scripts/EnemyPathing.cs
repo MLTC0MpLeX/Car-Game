8ad1dd267e72452ae4cc52b835dbdc7831a97125
MLTC0MpLeX <michaeljscerri@gmail.com>
2021-12-16 12:46:25 +0100

    Latest

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
+
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+        waypoints = waveConfig.GetWaypoints();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMove();
+    }
+    
+    void EnemyMove()
+    {
+        if (waypointIndex <= waypoints.Count -1)
+        {
+            //save the current waypoint in targetPosition
+            //targetPosition: where we want to go
+            var targetPosition = waypoints[waypointIndex].transform.position;
+    
+            //to make sure z position is 0	
+            targetPosition.z = 0f;
+    
+            var movementThisFrame = waveConfig.GetEnemyMoveSpeed() * Time.deltaTime;
+            //move from the current position, to the target position, the maximum distance one can move
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            //if we reached the target waypoint
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        //if enemy moved to all waypoints
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void SetWaveConfig(WaveConfig waveConfigToSet)
+    {
+        waveConfig = waveConfigToSet;
+    }
+
+
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the currentWave to the 1st Wave --> 0
+        var currentWave = waveConfigs[startingWave];
+
+        //start the coroutine that spawns all enemies in our currentWave
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        //spawns an enemy until enemyCount == GetNumberOfEnemies()
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            //spawn the enemy from 
+            //at the position specified by the waveConfig waypoints
+            var newEnemy = Instantiate(
+                waveConfig.GetEnemyPrefab(),
+                waveConfig.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+            //the wave will be selected from here and the enemy applied to it
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+
+        
+    }
+
+
+}
